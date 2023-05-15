36db227daac581f1e0ffeca6a07ec4ae5db50b93
raul prudente <raulprudente2016@icloud.com>
2023-05-15 14:51:18 -0400

    first commit

--- NetBeansProjects/red/src/red/Red.java
+++ NetBeansProjects/red/src/red/Red.java
@@ -4,27 +4,17 @@
  */
 package red;
 
-import java.util.Scanner;
+/**
+ *
+ * @author raulprudente
+ */
 public class Red {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        int total=0;
-        int gradecounter=1;
-        
-        while(gradecounter<=10){
-            System.out.print("enter grade:");
-            int grade=input.nextInt();
-            total=total+grade;
-            gradecounter=gradecounter+1;
-        }
-        int average=total/10;
-        System.out.printf("%n total of all grades is %d%n ",total);
-        System.out.printf("class average is %d%n ",average);
-        
+         System.out.println("hello world");
     }
     
 }
