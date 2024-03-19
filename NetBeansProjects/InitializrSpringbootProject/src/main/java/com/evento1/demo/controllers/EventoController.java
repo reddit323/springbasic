c420311918fd29b560d82eaabed2d2cd310646e0
raul prudente <raulprudente2016@icloud.com>
2024-03-19 10:31:10 -0400

    commit of spring framework

---
+++ NetBeansProjects/InitializrSpringbootProject/src/main/java/com/evento1/demo/controllers/EventoController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.evento1.demo.controllers;
+
+/**
+ *
+ * @author raulprudente
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class EventoController {
+    @RequestMapping("/cadastrarEvento")
+    
+    public String form(){
+        return "evento/formEvento";
+    }
+    
+}
